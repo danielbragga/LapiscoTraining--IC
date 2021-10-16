03d72f63069f11c9a0bec3ffb159345e7e025d4d
danielbragga <89660062+danielbragga@users.noreply.github.com>
2021-10-16 13:31:24 -0300

    Create answer.py

---
+++ Questions/15/answer.py
@@ -0,0 +1,19 @@
+import cv2
+
+# Iniciar a camera
+cap = cv2.VideoCapture(0)
+
+while 1:
+    # Capturando cada frame
+    ret, frame = cap.read()
+
+    # Converter o frame de bgr para cinza
+    grayscale_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    canny = cv2.Canny(grayscale_image, 30, 100)
+
+    # Mostrar o resultado
+    cv2.imshow('Video', canny)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
